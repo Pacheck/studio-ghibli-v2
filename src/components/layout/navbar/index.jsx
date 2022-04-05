fec968718d5c30e38ad91c94969882ea3d27d87c
Pedro Pacheco <pacheckdev@gmail.com>
2022-04-05 05:22:08 -0300

    refactor: change link to list tem, added pathname controle to set default page select

--- src/components/layout/navbar/index.jsx
+++ src/components/layout/navbar/index.jsx
@@ -1,16 +1,53 @@
-import LogoImg from '../../../assets/logo.png';
+import { useEffect, useState } from 'react';
+import { useLocation, Link } from 'react-router-dom';
+
+import LogoImg from '../../../assets/images/logo.png';
 import { Container, Logo, List, ListItem } from './styles'
 
-const listItems = ['Filmes', 'Personagens', 'Espécies', 'Locais', 'Veículos'];
+import { initialPath, especies, locais, personagens, veiculos } from './constants';
+
+const listItems = [
+  { path: initialPath, name: 'Filmes' }, 
+  { path: personagens, name: 'Personagens' }, 
+  { path: especies, name: 'Espécies' }, 
+  { path: locais, name: 'Locais' }, 
+  { path: veiculos, name: 'Veículos' }
+];
 
 export function Navbar() {
+  const [selectedNavItem, setSelectedNavItem] = useState(null);
+  const location = useLocation();
+  
+  useEffect(() => {
+    switch(location.pathname){
+      case personagens:
+        setSelectedNavItem('Personagens');
+        break;
+      case veiculos:
+        setSelectedNavItem('Veículos');
+        break;
+      case especies:
+        setSelectedNavItem('Espécies');
+        break;
+      case locais:
+        setSelectedNavItem('Locais');
+        break;
+      default:
+        setSelectedNavItem('Filmes');
+    }
+  }, [location.pathname, selectedNavItem])
+
   return (
     <Container role='navbar' aria-label='navbar'>
       <Logo src={LogoImg} alt="Ghibli studio" />
       <section>
-        <List>
+        <List aria-label="nav-list">
           {
-            listItems.map(listItemNome => <ListItem key={listItemNome}>{listItemNome}</ListItem>)
+            listItems.map(({ path, name }) => (
+              <ListItem key={name} aria-label="nav-item" currentPage={selectedNavItem === name}>
+                <Link to={path}>{name}</Link>
+              </ListItem>
+            ))
           }
         </List>
       </section>
