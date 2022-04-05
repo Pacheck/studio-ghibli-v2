f49d700296aed1e8955965c5c26623e32e98cf1c
Pedro Pacheco <pacheckdev@gmail.com>
2022-04-05 02:12:27 -0300

    feat: added BrowserRouter to index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 import { App } from './App';
 
@@ -9,8 +9,8 @@ ReactDOM.createRoot(
   document.getElementById('root')
 ).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Router>
       <App />
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>
 );
