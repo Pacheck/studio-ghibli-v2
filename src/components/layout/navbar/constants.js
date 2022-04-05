be1a5bf218036c7b42c470a08d316f15446fe56c
Pedro Pacheco <pacheckdev@gmail.com>
2022-04-05 05:21:10 -0300

    feat: created constants

---
+++ src/components/layout/navbar/constants.js
@@ -0,0 +1,5 @@
+export const initialPath = '/';
+export const personagens = '/personagens';
+export const especies = '/especies';
+export const locais = '/locais';
+export const veiculos = '/veiculos';
