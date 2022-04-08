1f3a8e7eb0f90e81c4e73d1c6414ec4ee2d9bb0c
Pedro Pacheco <pacheckdev@gmail.com>
2022-04-08 04:59:53 -0300

    feat: created useRequest

---
+++ src/hooks/useRequest.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react';
+import Axios from '../services/axiosInstance';
+
+export const useRequest = (url) => {
+  const [responseArray, setResponseArray] = useState([]);
+
+
+
+
+  useEffect(() => {
+    async function getData () {
+      try {
+        const response = await Axios.get(url);
+        console.log(response.data);
+        setResponseArray(response.data);
+      } catch (err) {
+        console.log(err);
+        return null;
+      }
+
+      return Axios.get(url);
+    }
+
+    getData();
+  }, [url]);
+
+  return [responseArray, setResponseArray];
+}
