e4bda5c1e5b6ba62371a759f468f7f8df9ee0ee9
Pedro Pacheco <pacheckdev@gmail.com>
2022-04-09 04:20:15 -0300

    feat: adding home

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,3 +1,23 @@
+import { Link } from 'react-router-dom';
+import { useRequest } from "../../hooks/useRequest"
+import { Card } from '../../components/card';
+import { Container, Wrapper } from "./styles";
+
 export function Home() {
-  return <h2>Home Page</h2>
+  const [filmsArray, setFilmsArray] = useRequest('/films');
+
+  return (
+    <Wrapper>
+      <Container>
+        {
+          filmsArray.length > 0 && filmsArray.map(film => (
+            <Card key={film.title}>
+              <img src={film.image} alt="movie"/>
+              <Link to="/">Ver mais</Link>
+            </Card>
+          ))
+        }
+      </Container>
+    </Wrapper>
+  )
 }
