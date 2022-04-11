b8b36488f3d46cdb8ac5892c1d4fb34d58ad1894
Pedro Pacheco <pacheckdev@gmail.com>
2022-04-10 22:46:54 -0300

    feat: finished card component

--- src/components/card/styles.js
+++ src/components/card/styles.js
@@ -2,45 +2,91 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   position: relative;
+  
+  a {
+    display: flex;
+    justify-content: center;
+    width: fit-content;
+    opacity: 0;
+    visibility: hidden;
+    padding: .7rem 2.5rem;
+    z-index: 10;
+    border-radius: 4px;
+    color: #e6e2e259;
+    background-color: transparent;
+    transition: all .3s ease-in;
+    transition: visibility 0s, opacity .5s linear;
+  }
+
   img {
     width: 100%;
     border-radius: 25px;
     box-shadow: 8px 6px 17px 7px rgb(0 0 0 / 38%);
     transition: all .3s ease-in;
     cursor: pointer;
+  }
 
-    :hover {
-      transform: scale(1.07);
+  :hover {
+    img {
+      transform: scale(1.1);
       box-shadow: 0px 12px 8px 4px rgba(0,0,0,0.33);
       border-radius: 4px;
-      border: 2px solid #80bdf0;;
+      border: 2px solid #80bdf0;
     }
 
-    @media(max-width: 418px){
-      :hover {
-        margin: 1rem 0;
+    a {
+      visibility: visible;
+      opacity: 1;
+      transform: scale(1.07);
+      animation: Resize .5s ease-in-out forwards;
+      background-color: rgba(30 29 29 / 90%);
+      color: #fff;
+      
+      :hover { 
+        background-color: rgba(0, 0, 0, 0.85);
       }
     }
   }
 
-  :hover a {
-    background-color: transparent;
-    transform: scale(1.07);
+  @media(max-width: 480px){
+    :hover {
+      margin: 1rem 0;
+    }
+
+    a {
+      padding: 1.5rem 2.5rem;
+      font-size: 1.5rem;
+    }
   }
 
-  //TODO arrumar o hover do ver mais
-  /* a {
-    color: red;
-    background-color: gray;
-    padding: .7rem 2.5rem;
-    position: absolute;
-    border-radius: 4px;
-    left: calc(50% - 69.14px);
-    bottom: 5%;
-    transition: all .3s ease-in;
-
+  @media(max-width: 768px){
     :hover {
-      transform: scale(1.07);
+      a {
+        visibility: visible;
+        opacity: 1;
+        transform: scale(1.07);
+        animation: Resize .5s ease-in-out forwards;
+        background-color: rgba(30 29 29 / 95%);
+        color: #fff; 
+      }
     }
-  } */
+  }
+
+  @keyframes Resize {
+    0% {
+      width: auto;
+    }
+    20% {
+      width: 50%;
+    }
+    30% {
+      width: 55%;
+    }
+    50% {
+      width: 60%;
+    }
+    100% {
+      width: 80%;
+    }
+  }
 `;
